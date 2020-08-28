d75c796ef2346e8c706b1cf358626cb55dc55363
booldook <booldook@gmail.com>
2020-08-28 11:49:56 +0900

    js 나타나 사라져

--- js/01.function.js
+++ js/01.function.js
@@ -1,6 +1,21 @@
 $("#bt1").click(function(){
-	$("#img1").hide();
+	$("#img1").stop().fadeOut(500);
 });
 $("#bt2").click(function(){
-	$("#img1").show();
+	$("#img1").stop().fadeIn(500);
+});
+$("#bt3").click(function(){
+	$("#img1").toggle();
+});
+$("#bt4").click(function(){
+	$("#img1").stop().fadeToggle(500);
+});
+$("#bt5").click(function(){
+	$("#img1").stop().slideUp(500);
+});
+$("#bt6").click(function(){
+	$("#img1").stop().slideDown(500);
+});
+$("#bt7").click(function(){
+	$("#img1").stop().slideToggle(500);
 });
