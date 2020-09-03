31b43deb4e939ddaa08097881ee2b8876d9d7c11
booldook <booldook@gmail.com>
2020-09-03 18:12:04 +0900

    200903 완료

--- js/05.lotto.js
+++ js/05.lotto.js
@@ -15,9 +15,24 @@ function onFinder() {
 */
 
 function onFinder() {
+	$(".lotto-wrap").empty();
+	var html, className;
 	var numbers = [];
+	var selectedNumbers = [];
 	for(var i=1; i<=45; i++) numbers.push(i);
-	console.log(numbers.shuffle());
+	numbers = _.shuffle(numbers);
+	for(var i=0; i<6; i++) selectedNumbers.push(numbers.pop());
+	selectedNumbers = selectedNumbers.sort(function(a, b) { return a - b });
+	//return b - a; // 내림차순
+	for(var i in selectedNumbers) {
+		if(selectedNumbers[i] < 11) className = 'yellow';
+		else if(selectedNumbers[i] >= 11 && selectedNumbers[i] < 21) className = 'cyan';
+		else if(selectedNumbers[i] >= 21 && selectedNumbers[i] < 31) className = 'red';
+		else if(selectedNumbers[i] >= 31 && selectedNumbers[i] < 41) className = 'grey';
+		else className = 'green';
+		html = '<div class="lotto '+className+'">'+selectedNumbers[i]+'</div>';
+		$(".lotto-wrap").append(html);
+	}
 }
 
 $("#btFinder").click(onFinder);
