b582f097e9bace1e3ebae18df775d99b489b1b37
booldook <booldook@gmail.com>
2020-09-04 13:14:51 +0900

    box만들기

---
+++ js/06.for.js
@@ -0,0 +1,18 @@
+function onHover() {
+	$(this).css("opacity", 0);
+}
+
+function onMaker() {
+	$(".wrapper").empty();
+	for(var i=0; i<2000; i++) {
+		$(".wrapper").append('<div class="box"></div>');
+	}
+	$(".box").mouseover(onHover);
+}
+
+function onReset() {
+	$(".wrapper").empty();
+}
+
+$("#btMaker").click(onMaker);
+$("#btReset").click(onReset);
